aaa7d2e8a7a13b53ede9aa0ca541b088f7a9ca1f
Ajay <ajajaj2807@gmail.com>
2021-05-10 13:21:18 +0530

    Basic layout for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-const Navbar = () => {
-    return(
-        <div>
-            Hello World!
-        </div>
-    )
-}
+import "../styles/Navbar.css";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <div className="links-group">
+        <p>Logo</p>
+        <a href="#">Live</a>
+        <a href="#">Push</a>
+        <a href="#">Link</a>
+        <a href="#">Shop</a>
+        <a href="#">Packs</a>
+        <a href="#">Help</a>
+      </div>
+      <div className="btn-group">
+        <a href="#">Try Live for free</a>
+        <a href="#">Login or register</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
